b51c0b530f944e49e5da16b199382a4bed87494d
Vinícius Kobal Fregati <vfregati@gmail.com>
2022-10-25 09:24:40 -0300

    Construindo a API

---
+++ FilmeAPI/Controllers/FilmeController.cs
@@ -0,0 +1,21 @@
+﻿using FilmeAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace FilmeAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody] Filme filme)
+        {
+            if (filme.Titulo != null && filme.Titulo != "" && filme.Duracao <= 600)
+                filmes.Add(filme);
+        }
+    }
+}
---
+++ FilmeAPI/Models/Filme.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmeAPI.Models
+{
+    public class Filme
+    {
+        [Required(ErrorMessage = "O campo título é obrigatório")] // Não aceita nulo ou vazio
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo diretor é obrigatório")]
+        public string Diretor { get; set; }
+        [StringLength(30, ErrorMessage = "O gênero não pode passar de 30 caracteres")]
+        public string Genero { get; set; }
+        [Range(1, 600, ErrorMessage = "A duração deve ter no mínimo 1 e no máximo 600 minutos")] // Só aceita valores entre esses
+        public int Duracao { get; set; }
+    }
+}
